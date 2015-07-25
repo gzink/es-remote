2ece4302e9e8bd85e2491c3d3899b6330cb8edf3
Gabriel Zink <info@gabrielzink.de>
2015-07-25 11:27:48 +0200

    elastic search, json

--- daemon.py
+++ daemon.py
@@ -1,4 +1,4 @@
-import ConfigParser, datetime, json, os
+import ConfigParser, datetime, json, os, pytz
 from flask import Flask, request
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
@@ -9,6 +9,8 @@ Config = ConfigParser.ConfigParser()
 Config.read(dirname + '/config.ini')
 
 elastic_host = Config.get("main", "elastic_host")
+elastic_index_name = Config.get("main", "elastic_index_prefix")
+elastic_show_name = Config.get("main", "elastic_show_name")
 
 es = Elasticsearch(elastic_host)
 
@@ -23,13 +25,30 @@ app.debug = True
 
 print getNameForApiKey("")
 
-@app.route("/<apiKey>", methods=['POST'])
-def put(apiKey):
-    now = datetime.datetime.now()
+@app.route("/<apiKey>/<_type>", methods=['POST'])
+def put(apiKey, _type):
+    now = datetime.datetime.now(pytz.timezone('Europe/Berlin'))
+    name = getNameForApiKey(apiKey)
+    if name is None:
+        return json.dumps({"error": "invalid api key"})
     try:
         decoded = json.loads(request.stream.read())
     except ValueError:
         return json.dumps({"error": "invalid json message"})
+    index = elastic_index_name + '-' + now.strftime('%Y-%m-%d')
+
+    if type(decoded) is dict:
+        decoded = [decoded]
+
+    for row in decoded:
+        row['@timestamp'] = now
+        row['_index'] = index
+        row['_type'] = _type
+        if elastic_show_name:
+            row['name'] = name
+
+    print helpers.bulk(es, decoded)
+
     return "Hello World!"
 
 if __name__ == "__main__":
